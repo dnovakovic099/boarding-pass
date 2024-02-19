7724be6778d30a2eca4a782ffdca99029238c953
nrtdevs <Dev@newrise.in>
2024-02-19 12:04:06 +0530

    Update LandingPage.tsx
    login done

--- src/Pages/LandingPage.tsx
+++ src/Pages/LandingPage.tsx
@@ -63,83 +63,87 @@ const LandingPage = () => {
 
   return (
     <>
-
-      <div className="h-screen flex flex-col p-2   bg-[#242424] ">
-        <div className="flex justify-center   ">
-          <div className="rounded-full bg-[#414141] w-24 size-24 justify-center flex items-center border-8 border-[#616161]">
+      <div className="min-h-screen flex flex-col p-6   bg-[#242424] ">
+        <div className="flex flex-col gap-5 justify-center items-center relative">
+          <div className="overflow-hidden rounded-[60px] border-[3px] justify-center flex items-center border-[#616161] bg-[#414141] relative w-[90px] h-[90px]">
             <VerifiedIcon />
           </div>
+          <div className="flex flex-col gap-3 justify-center items-center relative">
+            <h4 className="flex tracking-[0.024em] max-w-[350px] text-center font-semibold leading-[29px] text-2xl text-white">
+              Let's get your booking verified
+            </h4>
+            <p className="max-w-[350px] text-center leading-5 text-sm text-[#b1b1b1]">
+              Please provide the necessary details to ensure the verification of your booking.
+            </p>
+          </div>
         </div>
-        <form
-          className="flex flex-col items-center justify-between h-screen gap-4 sm:gap-8  sm:px-5 sm:px-[30%]"
-          onSubmit={handleSubmit}
-        >
-          <div className="flex flex-col gap-2.5  w-full">
-            <h1 className="text-2xl text-center my-2 text-white text-nowrap font-bold">Let’s get your booking verified</h1>
-            <p className="text-center my-2 text-[#B1B1B1] mx-4">Please provide the necessary details to ensure the verification of your booking.</p>
-            <div className="box-border h-32 w-100 p-4 ">
-              <div className="h-8 sm:h-10 w-full">
-                <input
-                  type="text"
-                  id="firstName"
-                  name="firstName"
-                  onChange={handleChange}
-                  value={formData.firstName}
-                  placeholder="First Name"
-                  className="peer h-14 w-full  sm:rounded-md sm:rounded-1 rounded-md  !border bg-[#414141] !border-gray-300 border-t-transparent bg-transparent pl-1.5 sm:pl-2.5 py-1 sm:py-3 font-sans text-base font-normal text-white outline outline-0 ring-4 ring-transparent transition-all placeholder:text-white placeholder-shown:border placeholder-shown:border-blue-gray-200 placeholder-shown:border-t-blue-gray-200 disabled:border-0 disabled:bg-blue-gray-50 leading-tight focus:outline-none focus:border-blue-500 cursor-pointer"
-                  required
-                />
-              </div>
-              <div className="h-8 sm:h-10 w-full mt-8">
-                <input
-                  type="text"
-                  id="lastName"
-                  name="lastName"
-                  onChange={handleChange}
-                  value={formData.lastName}
-                  placeholder="Last Name"
-                  className="peer h-14 w-full sm:rounded-md sm:rounded-1 rounded-md bg-[#414141] !border !border-gray-300 border-t-transparent bg-transparent pl-1.5 sm:pl-2.5 py-1 sm:py-3 font-sans text-base font-normal text-white outline outline-0 ring-4 ring-transparent transition-all placeholder:text-white placeholder-shown:border placeholder-shown:border-blue-gray-200 placeholder-shown:border-t-blue-gray-200 disabled:border-0 disabled:bg-blue-gray-50 leading-tight focus:outline-none focus:border-blue-500 cursor-pointer"
-                  required
-                />
-              </div>
-              <div className="h-8 sm:h-10 w-full mt-8">
-                <input
-                  type="text"
-                  id="email"
-                  name="email"
-                  onChange={handleChange}
-                  value={formData.email}
-                  placeholder="Email"
-                  className="peer h-14 w-full  sm:rounded-md sm:rounded-1 rounded-md !border !border-gray-300 border-t-transparent bg-[#414141] pl-1.5 sm:pl-2.5 py-1 sm:py-3 font-sans text-base font-normal text-white outline outline-0 ring-4 ring-transparent transition-all placeholder:text-white placeholder-shown:border placeholder-shown:border-blue-gray-200 placeholder-shown:border-t-blue-gray-200 disabled:border-0 disabled:bg-blue-gray-50 leading-tight focus:outline-none focus:border-blue-500 cursor-pointer"
-                  required
-                />
-              </div>
-              <div className="flex items-center w-full mt-8" onChange={handleChange}>
-                <label
-                  htmlFor="id-document"
-                  className="flex flex-col h-14 items-start rounded-md  justify-center w-full sm:rounded-md sm:rounded-1 bg-[#414141] cursor-pointer bg-transparent border pl-1.5 sm:pl-2.5 py-1 sm:py-3"
-                >
-                  <p className="text-base text-white">ID Document</p>
+        <div className="mx-4 mt-4">
+          <div className="flex  flex-col justify-center items-center">
+            <form onSubmit={handleSubmit}>
+              <div className="flex flex-col gap-5  items-start relative ">
+                <div className="mt-1" >
                   <input
-                    id="id-document"
-                    type="file"
-                    className="hidden"
-                    name="id-document"
+                    type="text"
+                    id="firstName"
+                    name="firstName"
+                    onChange={handleChange}
+                    value={formData.firstName}
+                    placeholder="First Name"
+                    className="rounded-md p-[14px]  bg-[#414141] border-none outline-none text-white flex border-0  gap-2.5 justify-start items-center relative w-[350px] h-[54px] placeholder:text-white"
+                    required
                   />
-                </label>
+                </div>
+                <div >
+                  <input
+                    type="text"
+                    id="lastName"
+                    name="lastName"
+                    onChange={handleChange}
+                    value={formData.lastName}
+                    placeholder="Last Name"
+                    className="rounded-md p-[14px] bg-[#414141] border-0 border-none outline-none text-white  flex gap-2.5 justify-start items-center relative w-[350px] h-[54px] placeholder:text-white"
+                    required
+                  />
+                </div>
+                <div >
+                  <input
+                    type="text"
+                    id="email"
+                    name="email"
+                    onChange={handleChange}
+                    value={formData.email}
+                    placeholder="Email"
+                    className="rounded-md p-[14px] flex gap-2.5 justify-start border-none outline-none text-white items-center border-0 bg-[#414141] relative w-[350px] h-[54px] placeholder:text-white"
+                    required
+                  />
+                </div>
+                <div className="flex items-center w-full" onChange={handleChange}>
+                  <label
+                    htmlFor="id-document"
+                    className="flex flex-col h-14 items-start rounded-md  justify-center w-full  bg-[#414141] cursor-pointer  pl-1.5  py-1 "
+                  >
+                    <p className="text-base text-white ">ID Document</p>
+                    <input
+                      id="id-document"
+                      type="file"
+                      className="hidden rounded-md border-0 bg-[#414141]"
+                      name="id-document"
+                    />
+                  </label>
+                </div>
               </div>
-
-              <div className="flex flex-col w-full mt-14 h-screen">
-                <p className="flex flex-col text-xs text-white">
-                  By clicking submit, you agree to our Privacy Policy
+              <div className="mt-5">
+                <p className=" text-center text-sm text-white">
+                  By clicking submit, you agree to our ?
+                  <a href="#" className="font-semibold leading-6 space-x-0 underline  border-l-indigo-500 text-white">Privacy Policy</a>
                 </p>
-                <button type="submit" title="Pay" disabled={false} onClick={openStripe} className="mt-14 BtnCustomgfhghColors w-full h-14 rounded-md px-10 py-15 text-lg">
+                <button type="submit" title="Pay" disabled={false} onClick={openStripe} className="mt-8 BtnCustomgfhghColors w-full h-14 font-bold rounded-md px-10 ">
                   Submit
                 </button>
               </div>
-            </div>
+            </form>
           </div>
-        </form>
+        </div>
       </div>
     </>
   );
